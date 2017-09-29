1670e60b5250ffcf7629bdcac83d0657a8a4058d
Truong Ha Giang <hagianga21@gmail.com>
2017-09-29 15:15:29 +0700

    Add vector Icons

--- MobileApp/SmartHome/src/Router.js
+++ MobileApp/SmartHome/src/Router.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {StackNavigator,DrawerNavigator} from 'react-navigation';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Dashboard from './Dashboard';
 import Control from './Control';
 import Scenes from './Scenes';
@@ -7,7 +8,12 @@ import Scenes from './Scenes';
 
 export const SideMenu = DrawerNavigator({
     Dashboard_Screen:{
-        screen: Dashboard
+        screen: Dashboard,
+        navigationOptions:{
+            drawerIcon: ({ tintColor }) => (
+                <MaterialIcons name="drafts" size={24} style={{ color: tintColor }} />
+            ),
+        }
     },
     Control_Screen:{
         screen: Control
