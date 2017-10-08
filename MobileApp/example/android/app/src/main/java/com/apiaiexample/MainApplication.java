4949ac06fa3aff10fae2beb995554517cf3a57bd
Truong Ha Giang <hagianga21@gmail.com>
2017-10-08 13:04:54 +0700

    Add Text to speech

--- MobileApp/example/android/app/src/main/java/com/apiaiexample/MainApplication.java
+++ MobileApp/example/android/app/src/main/java/com/apiaiexample/MainApplication.java
@@ -3,6 +3,7 @@ package com.apiaiexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import net.no_mad.tts.TextToSpeechPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import de.innfactory.apiai.RNApiAiPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new TextToSpeechPackage(),
             new VectorIconsPackage(),
             new RNApiAiPackage()
       );
--- MobileApp/example/src/Scenes.js
+++ MobileApp/example/src/Scenes.js
@@ -8,88 +8,20 @@ import {
 } from 'react-native';
 
 import ApiAi from "react-native-api-ai"
+import Tts from 'react-native-tts';
 
 export default class Sences extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        result: "",
-        listeningState: "not started",
-        audioLevel: 0,
+    Speak(){
+        Tts.speak('Hello, world!');
     };
 
-    console.log(ApiAi);
-
-    ApiAi.setConfiguration(
-        "27729321705749a0b6c8e92cb4812a97", ApiAi.LANG_ENGLISH_US
-    );
-
-
-
-    const contexts = [{
-        "name": "deals",
-        "lifespan": 1,
-        "parameters": {
-            "name": "Sam"
-        }
-    }];
-
-    ApiAi.setContexts(contexts);
-
-
-    const entities = [{
-        "name":"shop",
-        "extend":true,
-        "entries":[
-            {
-                "value":"Media Markt",
-                "synonyms":[
-                    "Media Markt",
-                ]
-            }
-        ]
-    }];
-
-
-    ApiAi.setEntities(entities);
-  }
   render() {
     return (
         <View style={styles.container}>
 
-            <View style={{flex: 4}}>
-                <Text>{"Listening State: " + this.state.listeningState}</Text>
-                <Text>{"Audio Level: " + this.state.audioLevel}</Text>
-                <Text>{"Result: " + this.state.result}</Text>
-            </View>
             <View style={{flex: 1, padding: 10}}>
                 <Button title="Start Listening" onPress={() => {
-
-
-                    ApiAi.onListeningStarted(() => {
-                        this.setState({listeningState: "started"});
-                    });
-
-                    ApiAi.onListeningCanceled(() => {
-                        this.setState({listeningState: "canceled"});
-                    });
-
-                    ApiAi.onListeningFinished(() => {
-                        this.setState({listeningState: "finished"});
-                    });
-
-                    ApiAi.onAudioLevel(level => {
-                        this.setState({audioLevel: level});
-                    });
-
-                    ApiAi.startListening(result => {
-            console.log(result);
-                        this.setState({result: result});
-                    }, error => {
-                        this.setState({result: error});
-                    });
-
+                    this.Speak();
                 }}/>
             </View>
         </View>
